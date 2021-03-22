f37094473abe0da74842eb609da03d9b5f9f0ded
junqing258@gmail.com <junqing258@gmail.com>
2021-03-22 18:11:47 +0800

    Update usdt_validator.js

--- src/usdt_validator.js
+++ src/usdt_validator.js
@@ -2,9 +2,9 @@ var BTCValidator = require('./bitcoin_validator');
 var ETHValidator = require('./ethereum_validator');
 
 function checkBothValidators(address, currency, networkType) {
-    var result = BTCValidator.isValidAddress(address, currency, networkType);
-    return result ? result :
-        ETHValidator.isValidAddress(address, currency, networkType);
+    return ETHValidator.isValidAddress(address, currency, networkType) ||
+        TRXValidator.isValidAddress(address, currency, networkType) ||
+        BTCValidator.isValidAddress(address, currency, networkType);
 }
 
 module.exports = {
@@ -12,6 +12,8 @@ module.exports = {
         if (opts) {
             if (opts.chainType === 'erc20') {
                 return ETHValidator.isValidAddress(address, currency, opts.networkType);
+            } else if (opts.chainType === "trc20") {
+                return TRXValidator.isValidAddress(address, currency, opts.networkType);
             } else if (opts.chainType === 'omni') {
                 return BTCValidator.isValidAddress(address, currency, opts.networkType);
             }
